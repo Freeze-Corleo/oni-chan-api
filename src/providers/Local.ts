a6d41c53ae8d028201f5d7e24e1c32acd356555c
LeoDELPON <delponleo@gmail.com>
2022-06-07 14:23:23 +0200

    Implement Api key check

--- src/providers/Local.ts
+++ src/providers/Local.ts
@@ -4,28 +4,30 @@
  * @author Léo DELPON <leo.delpon@viacesi.fr>
  */
 
- import { Application } from 'express';
- import * as path from 'path';
- import * as dotenv from 'dotenv';
+import { Application } from 'express';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
 
- class Locals {
-     /**
-      * Makes env configs available for your app
-      * throughout the app's runtime
-      */
-     public static config(): any {
-         dotenv.config({ path: path.join(__dirname, '../../.env') });
-         const monitorName = process.env.STATUS_MONITOR_NAME || 'status_monitor';
-         const url = process.env.APP_URL || `http://localhost:${process.env.PORT}`;
-         const port = process.env.PORT || 8080;
-         const mongooseUrl = process.env.MONGOOSE_URL;
-         const mslHost = process.env.HOST_MS;
-         const msUsername = process.env.USERNAME_MS;
-         const msPassword = process.env.PASSWORD_MS;
-         const apiPrefix = process.env.API_PREFIX || 'oni-chan';
-         const appSecret = process.env.APP_SECRET || 'test-secret-string-for-session';
+class Locals {
+    /**
+     * Makes env configs available for your app
+     * throughout the app's runtime
+     */
+    public static config(): any {
+        dotenv.config({ path: path.join(__dirname, '../../.env') });
+        const monitorName = process.env.STATUS_MONITOR_NAME || 'status_monitor';
+        const url = process.env.APP_URL || `http://localhost:${process.env.PORT}`;
+        const port = process.env.PORT || 8080;
+        const mongooseUrl = process.env.MONGOOSE_URL;
+        const mslHost = process.env.HOST_MS;
+        const apiKey = process.env.API_KEY;
+        const msUsername = process.env.USERNAME_MS;
+        const msPassword = process.env.PASSWORD_MS;
+        const apiPrefix = process.env.API_PREFIX || 'oni-chan';
+        const appSecret = process.env.APP_SECRET || 'test-secret-string-for-session';
 
-         return {
+        return {
+            apiKey,
             monitorName,
             appSecret,
             mongooseUrl,
@@ -34,17 +36,17 @@
             msPassword,
             apiPrefix,
             port,
-            url,
-         };
-     }
+            url
+        };
+    }
 
-     /**
-      * Injects your config to the app's locals
-      */
-     public static init(_express: Application): Application {
-         _express.locals.app = this.config();
-         return _express;
-     }
- }
+    /**
+     * Injects your config to the app's locals
+     */
+    public static init(_express: Application): Application {
+        _express.locals.app = this.config();
+        return _express;
+    }
+}
 
- export default Locals;
+export default Locals;
