f541f8304379c8a4e1e318a0e0b7b23880daa11d
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2022-06-28 13:37:13 +0200

    Socket try

--- src/middlewares/Socket.ts
+++ src/middlewares/Socket.ts
@@ -4,24 +4,38 @@
  * @author Léo DELPON <leo.delpon@viacesi.fr>
  */
 
-
 import { Application } from 'express';
 import Log from './Log';
 import Local from '../providers/Local';
 
-
 class Socket {
-  public socketio = require('socket.io');
-  constructor() {
-  }
+    public socketio = require('socket.io');
+    constructor() {}
 
+    public mountSocketServer(_express: Application) {
+        Log.info('Socket :: Mounting Socket server in API');
 
-  public mountSocketServer(_express: Application) {
-    Log.info('Socket :: Mounting Socket server in API');
+        const io = this.socketio(_express);
+        let interval;
 
-    this.socketio(_express);
-  }
+        io.on('connection', (socket) => {
+            console.log('New client connected');
+            if (interval) {
+                clearInterval(interval);
+            }
+            interval = setInterval(() => getApiAndEmit(socket), 1000);
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+                clearInterval(interval);
+            });
+        });
 
+        const getApiAndEmit = (socket) => {
+            const response = new Date();
+            // Emitting a new message. Will be consumed by the client
+            socket.emit('FromAPI', response);
+        };
+    }
 }
 
-export default new Socket;
+export default new Socket();
