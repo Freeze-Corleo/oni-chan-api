f4f3bc069e6ec04bef61660036a20e7b92c5dcb6
LeoDELPON <delponleo@gmail.com>
2022-07-01 10:55:05 +0200

    Intention deploy

--- src/middlewares/CORS.ts
+++ src/middlewares/CORS.ts
@@ -4,26 +4,29 @@
  * @author Léo DELPON <leo.delpon@viacesi.fr>
  */
 
- import { Application } from 'express';
+import { Application } from 'express';
 
- import Log from './Log';
- import Locals from '../providers/Local';
+import Log from './Log';
+import Locals from '../providers/Local';
 
- class CORS {
-    public cors = require('cors')
-     public mount(_express: Application): Application {
-         Log.info('CORS :: Booting the \'CORS\' middleware...');
+class CORS {
+    public cors = require('cors');
+    public mount(_express: Application): Application {
+        Log.info("CORS :: Booting the 'CORS' middleware...");
 
-         const options = {
-             origin: "*",
-             transports: ['websocket', 'polling'],
-             optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-         };
+        const options = {
+            origin: [
+                'https://oni-chan-dashbard.vercel.app,',
+                'https://test-onichan-api.herokuapp.com'
+            ],
+            transports: ['websocket', 'polling'],
+            optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+        };
 
-         _express.use(this.cors(options));
+        _express.use(this.cors(options));
 
-         return _express;
-     }
- }
+        return _express;
+    }
+}
 
- export default new CORS;
+export default new CORS();
