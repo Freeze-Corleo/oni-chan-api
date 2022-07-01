f673ce296476f6caa621c3fcf9dd20b5fa5454be
LeoDELPON <delponleo@gmail.com>
2022-07-01 11:11:34 +0200

    Intention deploy

--- src/middlewares/CORS.ts
+++ src/middlewares/CORS.ts
@@ -14,16 +14,16 @@ class CORS {
     public mount(_express: Application): Application {
         Log.info("CORS :: Booting the 'CORS' middleware...");
 
-        const options = {
-            origin: [
-                'https://oni-chan-dashbard.vercel.app,',
-                'https://test-onichan-api.herokuapp.com'
-            ],
-            transports: ['websocket', 'polling'],
-            optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-        };
+        // const options = {
+        //     origin: [
+        //         'https://oni-chan-dashbard.vercel.app,',
+        //         'https://test-onichan-api.herokuapp.com'
+        //     ],
+        //     transports: ['websocket', 'polling'],
+        //     optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+        // };
 
-        _express.use(this.cors(options));
+        // _express.use(this.cors(options));
 
         return _express;
     }
